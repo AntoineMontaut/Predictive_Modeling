6e32930ca11d70e8d39d566cb57540ff3186798e
AntoineMontaut <antoine.montaut@gmail.com>
2016-10-22 18:04:37 -0500

    update

--- smartphone_activity.py
+++ smartphone_activity.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 import re
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import precision_score
+import sklearn.metrics as skmet
 
 activity_list = ['Walking', 'Walking upstairs', 'Walking downstairs',
                       'Sitting', 'Standing', 'Laying']
+activity_list_abr = ['Walk', 'Walk up', 'Walk down',
+                      'Sit', 'Stand', 'Lay']
 activity_dict = {'1': 'Walking', '2': 'Walking upstairs', '3': 'Walking downstairs',
                        '4': 'Sitting', '5': 'Standing', '6': 'Laying'}
 
@@ -92,14 +93,13 @@ X_train = X_train[to_keep]
 
 
 '''TRAIN RANDOM FOREST ON TRAINING DATA'''
-clf = RandomForestClassifier(n_estimators=500)
+print('\n\n-----DOMAIN KNOWLEDGE APPROACH-----\n')
+clf = RandomForestClassifier(n_estimators=50)
 clf = clf.fit(X_train, y_train.activity)
 score_train = clf.score(X_train, y_train.activity)
 print('Random Forest accuracy on training data: {0}'.format(round(score_train, 5)))
-print('oob score: {0}',format(round(clf.oob_score_, 3)))
 # score_train = cross_val_score(clf, X_train, y_train.activity)
 # print('Random Forest mean score on training data: {0}'.format(round(score_train.mean(), 3)))
-
 feat_imp = pd.Series(clf.feature_importances_, index=X_train.columns)
 feat_imp_sorted = feat_imp.sort_values(ascending=False)
 print('Most important features:')
@@ -118,18 +118,74 @@ y_test['activity'] = y_test.activity_cat.map(lambda x: activity_dict[x])
 
 prediction = clf.predict(X_test)
 score_test = clf.score(X_test, y_test.activity)
-precision_test = precision_score(y_test.activity, prediction, average='micro')
-recall_test = recall_score(y_test.activity, prediction, average='micro')
-scores_rouned = map(lambda x: round(x, 5), [score_test, precision_test, recall_test])
-print('\nRandom Forest scores on test data (global):\n\tAccuracy: {0}\n\tPrecision: {1}\n\tRecall: {2}'.format(
-         scores_rouned[0], scores_rouned[1], scores_rouned[2]))
-precision_test = precision_score(y_test.activity, prediction, average='macro')
-recall_test = recall_score(y_test.activity, prediction, average='macro')
-scores_rouned = map(lambda x: round(x, 5), [score_test, precision_test, recall_test])
-print('\nRandom Forest scores on test data (per label and unweigthed averaged):\n\tAccuracy: {0}\n\tPrecision: {1}\n\tRecall: {2}'.format(
-         scores_rouned[0], scores_rouned[1], scores_rouned[2]))
-precision_test = precision_score(y_test.activity, prediction, average='weighted')
-recall_test = recall_score(y_test.activity, prediction, average='weighted')
-scores_rouned = map(lambda x: round(x, 5), [score_test, precision_test, recall_test])
-print('\nRandom Forest scores on test data (per label and weigthed averaged):\n\tAccuracy: {0}\n\tPrecision: {1}\n\tRecall: {2}'.format(
-         scores_rouned[0], scores_rouned[1], scores_rouned[2]))
+precision_test = skmet.precision_score(y_test.activity, prediction, average='micro')
+recall_test = skmet.recall_score(y_test.activity, prediction, average='micro')
+f1_test = skmet.f1_score(y_test.activity, prediction, average='micro')
+scores_rouned = map(lambda x: round(x, 5), [score_test, precision_test, recall_test, f1_test])
+print('\nRandom Forest scores on test data (global):\n\tAccuracy: {0}\n\tPrecision: {1}\n\tRecall: {2}\n\tF1: {3}'.format(
+         scores_rouned[0], scores_rouned[1], scores_rouned[2], scores_rouned[3]))
+precision_test = skmet.precision_score(y_test.activity, prediction, average='macro')
+recall_test = skmet.recall_score(y_test.activity, prediction, average='macro')
+f1_test = skmet.f1_score(y_test.activity, prediction, average='macro')
+scores_rouned = map(lambda x: round(x, 5), [score_test, precision_test, recall_test, f1_test])
+print('\nRandom Forest scores on test data (per label and unweigthed averaged):\n\tAccuracy: {0}\n\tPrecision: {1}\n\tRecall: {2}\n\tF1: {3}'.format(
+         scores_rouned[0], scores_rouned[1], scores_rouned[2], scores_rouned[3]))
+precision_test = skmet.precision_score(y_test.activity, prediction, average='weighted')
+recall_test = skmet.recall_score(y_test.activity, prediction, average='weighted')
+f1_test = skmet.f1_score(y_test.activity, prediction, average='weighted')
+scores_rouned = map(lambda x: round(x, 5), [score_test, precision_test, recall_test, f1_test])
+print('\nRandom Forest scores on test data (per label and weigthed averaged):\n\tAccuracy: {0}\n\tPrecision: {1}\n\tRecall: {2}\n\tF1: {3}'.format(
+         scores_rouned[0], scores_rouned[1], scores_rouned[2], scores_rouned[3]))
+         
+conf_matrix = pd.DataFrame(skmet.confusion_matrix(y_test.activity, prediction), columns=activity_list_abr, index=activity_list_abr)
+print('\nConfusion matrix:')
+print(conf_matrix)
+         
+
+'''BLACK BOX APPROACH'''
+print('\n\n-----BLACK BOX APPROACH-----\n')
+X_train_bb =pd.read_table('./UCI HAR Dataset/train/X_train.txt', sep ='\s+', names=features)
+#we could use xn for column names but because we don't take anything out of X_train so it doesn't make a difference
+clf_bb = RandomForestClassifier(n_estimators=50)
+clf_bb.fit(X_train_bb, y_train.activity)
+score_train = clf_bb.score(X_train_bb, y_train.activity)
+print('Random Forest accuracy on training data: {0}'.format(round(score_train, 5)))
+# score_train = cross_val_score(clf, X_train, y_train.activity)
+# print('Random Forest mean score on training data: {0}'.format(round(score_train.mean(), 3)))
+feat_imp = pd.Series(clf_bb.feature_importances_, index=X_train_bb.columns)
+feat_imp_sorted = feat_imp.sort_values(ascending=False)
+print('Most important features:')
+print(feat_imp_sorted[:10])
+
+X_test_bb =pd.read_table('./UCI HAR Dataset/test/X_test.txt', sep ='\s+', names=features)
+open_y_test = open('./UCI HAR Dataset/test/y_test.txt')
+y_test = []
+for line in open_y_test.readlines():
+    y_test.append(str(line).rstrip()[0])
+y_test = pd.DataFrame(y_test, columns=['activity_cat'])
+y_test['activity'] = y_test.activity_cat.map(lambda x: activity_dict[x])
+
+prediction = clf_bb.predict(X_test_bb)
+score_test = clf_bb.score(X_test_bb, y_test.activity)
+precision_test = skmet.precision_score(y_test.activity, prediction, average='micro')
+recall_test = skmet.recall_score(y_test.activity, prediction, average='micro')
+f1_test = skmet.f1_score(y_test.activity, prediction, average='micro')
+scores_rouned = map(lambda x: round(x, 5), [score_test, precision_test, recall_test, f1_test])
+print('\nRandom Forest scores on test data (global):\n\tAccuracy: {0}\n\tPrecision: {1}\n\tRecall: {2}\n\tF1: {3}'.format(
+         scores_rouned[0], scores_rouned[1], scores_rouned[2], scores_rouned[3]))
+precision_test = skmet.precision_score(y_test.activity, prediction, average='macro')
+recall_test = skmet.recall_score(y_test.activity, prediction, average='macro')
+f1_test = skmet.f1_score(y_test.activity, prediction, average='macro')
+scores_rouned = map(lambda x: round(x, 5), [score_test, precision_test, recall_test, f1_test])
+print('\nRandom Forest scores on test data (per label and unweigthed averaged):\n\tAccuracy: {0}\n\tPrecision: {1}\n\tRecall: {2}\n\tF1: {3}'.format(
+         scores_rouned[0], scores_rouned[1], scores_rouned[2], scores_rouned[3]))
+precision_test = skmet.precision_score(y_test.activity, prediction, average='weighted')
+recall_test = skmet.recall_score(y_test.activity, prediction, average='weighted')
+f1_test = skmet.f1_score(y_test.activity, prediction, average='weighted')
+scores_rouned = map(lambda x: round(x, 5), [score_test, precision_test, recall_test, f1_test])
+print('\nRandom Forest scores on test data (per label and weigthed averaged):\n\tAccuracy: {0}\n\tPrecision: {1}\n\tRecall: {2}\n\tF1: {3}'.format(
+         scores_rouned[0], scores_rouned[1], scores_rouned[2], scores_rouned[3]))
+         
+conf_matrix = pd.DataFrame(skmet.confusion_matrix(y_test.activity, prediction), columns=activity_list_abr, index=activity_list_abr)
+print('\nConfusion matrix:')
+print(conf_matrix)
