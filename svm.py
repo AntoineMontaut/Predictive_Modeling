3c4063463ef879da918dfc4c9cba47e4fd8d8830
AntoineMontaut <antoine.montaut@gmail.com>
2016-11-02 20:54:49 -0500

    update svm.py

--- svm.py
+++ svm.py
@@ -6,6 +6,11 @@ import matplotlib.pyplot as plt
 from sklearn import svm
 from matplotlib.colors import ListedColormap
 import numpy as np
+import itertools
+from matplotlib import gridspec
+
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
 iris = datasets.load_iris()
 # print(iris['target_names'])
@@ -14,12 +19,14 @@ iris = datasets.load_iris()
 # print(iris['feature_names'])
 # print(iris['DESCR'])
 
-df = pd.DataFrame(iris['data'], columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid'])
+features = ['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid']
+
+df = pd.DataFrame(iris['data'], columns=features)
 df['target'] = iris['target']
 df['iris'] = df.target.map(lambda x: iris['target_names'][x])
 # print(df.head())
 # print(df.tail())
-print(df.info())
+# print(df.info())
 
 def plot_data():
     
@@ -52,12 +59,10 @@ def plot_data():
 X = iris.data[0:100, :]
 y = iris.target[0:100]
 
-svc = svm.SVC(kernel='linear')
+svc = svm.SVC(kernel='linear', C=1)
 # svc.fit(X, y)
 
 def plot_estimator(estimator, X, y, title):
-    cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
-    cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
     estimator.fit(X, y)
     x_min, x_max = X[:, 0].min() - .1, X[:, 0].max() + .1
@@ -78,10 +83,49 @@ def plot_estimator(estimator, X, y, title):
     plt.tight_layout()
     # plt.show()
 
-plot_estimator(svc, X, y, 'Setosa & Versicolor')
+# plot_estimator(svc, X, y, 'Setosa & Versicolor')
 
-X = iris.data[50:, :]
-y = iris.target[50:]
-plot_estimator(svc, X, y, 'Versicolor & Virginica')
+# X = iris.data[50:, :]
+# y = iris.target[50:]
+# plot_estimator(svc, X, y, 'Versicolor & Virginica')
+
+df1 = df[df.iris != 'virginica']
+df2 = df[df.iris != 'versicolor']
+df3 = df[df.iris != 'setosa']
+combinations_iter = itertools.combinations(features, 2)
+combinations = []
+for com in combinations_iter:
+    combinations.append(com)
+
+def plot_all():
+    for df_sub in [df1, df2, df3]:
+        fig = plt.figure(' and '.join(df_sub.iris.unique()), figsize=(13, 8))
+        gs = gridspec.GridSpec(2, 3)
+        
+        i = -1
+        for combination in combinations:
+            i += 1
+            axes = plt.subplot(gs[i/3, i%3])
+            
+            svc.fit(df_sub[list(combination)], df_sub['target'])
+            x_min, x_max = df_sub[combination[0]].min() - .1, df_sub[combination[0]].max() + .1
+            y_min, y_max = df_sub[combination[1]].min() - .1, df_sub[combination[1]].max() + .1
+            xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100), np.linspace(y_min, y_max, 100))
+            Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
+
+            # Put the result into a color plot
+            Z = Z.reshape(xx.shape)
+            axes.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+            # Plot also the training points
+            plt.scatter(df_sub[combination[0]], df_sub[combination[1]], c=df_sub['target'], cmap=cmap_bold)
+            # axes.scatter(df_sub[combination[0]], df_sub[combination[1]], c=df_sub['target'], cmap=cmap_bold)
+            
+            plt.axis('tight')
+            # plt.axis('off')
+            plt.xlabel(combination[0])
+            plt.ylabel(combination[1])
+        
+plot_all()
 
 plt.show()
