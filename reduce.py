37cc686d1fbd5ee192001e4699cab4575d1a9764
AntoineMontaut <antoine.montaut@gmail.com>
2016-11-06 17:42:39 -0600

    update reduce.py

--- reduce.py
+++ reduce.py
@@ -41,9 +41,9 @@ df_X_lda = pd.DataFrame(lda.transform(df_X_std))
 
 def plot_pca_lda():
     colors = ['navy', 'turquoise', 'darkorange']
-    fig = plt.figure('PCA and LDA (2 components) applied to iris data', figsize=(13,9))
+    fig = plt.figure('PCA and LDA (2 components) applied to iris data', figsize=(13,11))
     
-    plt.subplot(2, 2, 1)
+    plt.subplot(3, 2, 1)
     for color, i, target_name in zip(colors, [0, 1, 2], target_names):
         plt.scatter(df_X_std[df_y==i]['petal_len'], df_X_std[df_y==i]['petal_wid'], color=color, label=target_name)
     plt.legend(loc='lower right', shadow=False, scatterpoints=1)
@@ -51,22 +51,40 @@ def plot_pca_lda():
     plt.xlabel('Petal Length (cm)')
     plt.ylabel('Petal Width (cm)')
     
-    plt.subplot(2, 2, 3)
+    plt.subplot(3, 2, 3)
     for color, i, target_name in zip(colors, [0, 1, 2], target_names):
         plt.scatter(df_X_pca[df_y==i][0], df_X_pca[df_y==i][1], color=color, label=target_name)
-    # plt.legend(loc='best', shadow=False, scatterpoints=1)
+    plt.legend(loc='best', shadow=False, scatterpoints=1)
     plt.title('PCA')
     
-    plt.subplot(2, 2, 4)
+    plt.subplot(3, 2, 4)
     for color, i, target_name in zip(colors, [0, 1, 2], target_names):
         plt.scatter(df_X_lda[df_y==i][0], df_X_lda[df_y==i][1], color=color, label=target_name)
-    # plt.legend(loc='best', shadow=False, scatterpoints=1)
+    plt.legend(loc='best', shadow=False, scatterpoints=1)
     plt.title('LDA')
     
-    plt.tight_layout()
-    plt.show()
     
-# plot_pca_lda()
+    colors = {'0': 'r', '1': 'b', '2': 'g'}
+    plt.subplot(3, 2, 5)
+    centroid, label_pca = cluster.vq.kmeans2(df_X_pca, 3)
+    label_pca = pd.Series(label_pca)
+    for i, target_name in zip([0, 1, 2], target_names):
+        plt.scatter(df_X_pca[label_pca==i][0], df_X_pca[label_pca==i][1], color=colors[str(i)], label=target_name)
+    # plt.legend(loc='best', shadow=False, scatterpoints=1)
+    plt.title('Clustering after PCA')
+    
+    colors = {'0': 'r', '1': 'b', '2': 'g'}
+    plt.subplot(3, 2, 6)
+    centroid, label_lda = cluster.vq.kmeans2(df_X_lda, 3)
+    label_lda = pd.Series(label_lda)
+    for i, target_name in zip([0, 1, 2], target_names):
+        plt.scatter(df_X_lda[label_lda==i][0], df_X_lda[label_lda==i][1], color=colors[str(i)], label=target_name)
+    # plt.legend(loc='best', shadow=False, scatterpoints=1)
+    plt.title('Clustering after LDA')
+    
+    plt.tight_layout()
+    
+plot_pca_lda()
 
 def plot_explained_variance_ratio():
     pca_max_comp = PCA(n_components=len(df_X_std.columns))
@@ -80,132 +98,7 @@ def plot_explained_variance_ratio():
     plt.xlim((.5, 4.5))
     plt.xticks((1, 2, 3, 4))
     plt.tight_layout()
-    plt.show()
     
 # plot_explained_variance_ratio()
-
-
-
-'''------------------------------------------ Apply PCA to UN dataset from lesson 5 ------------------------------------------'''
-
-
-df_UN_0 = pd.read_csv('un/un.csv')
-df_UN = df_UN_0[['lifeMale', 'lifeFemale', 'infantMortality', 'GDPperCapita']]
-df_UN = df_UN.dropna()
-features = cluster.vq.whiten(df_UN[['lifeMale', 'lifeFemale', 'infantMortality']])
-# print(df_UN.info())
-# print('Number of countries in the dataset: {0}'.format(len(df.country.unique())))
-
-x_columns = list(df_UN.columns)
-# [x_columns.remove(elem) for elem in ['GDPperCapita', 'country', 'region']]
-x_columns = ['lifeMale', 'lifeFemale', 'infantMortality']
-
-df_UN_X = df_UN[x_columns]
-df_UN_X_std = pd.DataFrame(StandardScaler().fit_transform(df_UN_X), columns=x_columns)
-df_UN_y = pd.Series(df_UN['GDPperCapita'])
-
-centroid, label = cluster.vq.kmeans2(features, 3)
-df_UN['label'] = label
-
-def plot_explained_variance_ratio_UN():
-    pca_max_comp = PCA(n_components=len(df_UN_X_std.columns))
-    pca_max_comp.fit(df_UN_X_std)
-    n_components = [x + 1 for x in range(len(df_UN_X_std.columns))]
-    
-    plt.figure('PCA\'s Explained Variance Ratio Using Max Number of Components', figsize=(8, 5))
-    plt.plot(n_components, pca_max_comp.explained_variance_ratio_, marker='o', c='r')
-    plt.xlabel('Component #')
-    plt.ylabel('Explained Variance Ratio')
-    # plt.xlim((.5, 4.5))
-    plt.xticks(range(1, 12))
-    plt.tight_layout()
-    plt.show()
-    
-# plot_explained_variance_ratio_UN()
-
-obs = ['lifeMale', 'lifeFemale', 'infantMortality', 'GDPperCapita']
-
-def plot_UN():
-    fig = plt.figure('lifeMale, lifeFemale, and infantMortality vs. GDPperCapita, colored by cluster')
-    color = ['b', 'r', 'g']
-    i=0
-    for ob in obs[:-1]:
-        i += 1
-        plt.subplot(2, 2, i)
-        for lab in xrange(3):
-            plt.scatter(df_UN[df_UN.label==lab].GDPperCapita, 
-                           df_UN[df_UN.label==lab][ob], c=color[lab])
-        plt.xlim([0,50000])
-        plt.xlabel('GDP per Capita')
-        plt.ylabel(ob)
-
-    plt.tight_layout()
-
-plot_UN()
-    
-def plot_UN_PCA():
-    pca_UN = PCA(n_components=3)
-    pca_UN.fit(df_UN_X_std)
-    df_UN_X_pca = pd.DataFrame(pca_UN.transform(df_UN_X_std))
-    centroid_pca, label_pca = cluster.vq.kmeans2(df_UN_X_pca[[0, 1, 2]], 3)
-    df_UN['label_pca'] = label_pca
-    label_pca = pd.Series(label_pca)
-    
-    fig = plt.figure('First 3 PCA components vs. GDPperCapita, colored by cluster\n(Clustering AFTER PCA)')
-    color = ['b', 'r', 'g']
-    i=0
-    for ob in xrange(3):
-        i += 1
-        plt.subplot(2, 2, i)
-        for lab in xrange(3):
-            plt.scatter(df_UN[df_UN.label_pca==lab].GDPperCapita, 
-                           df_UN_X_pca[label_pca==lab][ob], c=color[lab])
-        plt.xlim([0,50000])
-        plt.xlabel('GDP per Capita')
-        plt.ylabel('Component #{0}'.format(ob+1))
-
-    plt.tight_layout()
-    
-    fig = plt.figure('First 3 PCA components vs. GDPperCapita, colored by cluster\n(Clustering BEFORE PCA)')
-    color = ['b', 'r', 'g']
-    i=0
-    for ob in xrange(3):
-        i += 1
-        plt.subplot(2, 2, i)
-        for lab in xrange(3):
-            plt.scatter(df_UN[df_UN.label==lab].GDPperCapita, 
-                           df_UN_X_pca[label==lab][ob], c=color[lab])
-        plt.xlim([0,50000])
-        plt.xlabel('GDP per Capita')
-        plt.ylabel('Component #{0}'.format(ob+1))
-
-    plt.tight_layout()
-    
-plot_UN_PCA()
-
-def plot_UN_LDA():
-    lda_UN = LinearDiscriminantAnalysis(n_components=3)
-    lda_UN.fit(df_UN_X_std, df_UN.label)
-    df_UN_X_lda = pd.DataFrame(lda_UN.transform(df_UN_X_std))
-    centroid_lda, label_lda = cluster.vq.kmeans2(df_UN_X_lda[[0, 1]], 3)
-    df_UN['label_lda'] = label_lda
-    label_lda = pd.Series(label_lda)
-    
-    fig = plt.figure('First 3 LDA components vs. GDPperCapita, colored by cluster')
-    color = ['b', 'r', 'g']
-    i=0
-    for ob in xrange(2):
-        i += 1
-        plt.subplot(1, 2, i)
-        for lab in xrange(3):
-            plt.scatter(df_UN[df_UN.label==lab].GDPperCapita, 
-                           df_UN_X_lda[label==lab][ob], c=color[lab])
-        plt.xlim([0,50000])
-        plt.xlabel('GDP per Capita')
-        plt.ylabel('Component #{0}'.format(ob+1))
-
-    plt.tight_layout()
-    
-plot_UN_LDA()
 
 plt.show()
